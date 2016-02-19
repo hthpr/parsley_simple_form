d39250db4bbe4d7faf0c54b7743638948d694eea
Michael Tratz <michael@esosoft.com>
2016-02-19 14:38:48 -0800

    Added bootstrap classes for parsley.

--- lib/parsley_simple_form/simple_form_extensions/form_helper.rb
+++ lib/parsley_simple_form/simple_form_extensions/form_helper.rb
@@ -4,9 +4,17 @@ module ParsleySimpleForm
       include SimpleForm::ActionViewExtensions::FormHelper
 
       # Custom helper to SimpleForm
-      def parsley_simple_for(resource, *args, &block)
+      def parsley_simple_for(object, *args, &block)
         options = args.extract_options!
-        simple_form_for(resource, *(args << options.merge(builder: ParsleySimpleForm::SimpleFormAdapt, html: { :"parsley-validate" => true })), &block)
+        options[:builder] ||= ParsleySimpleForm::SimpleFormAdapt
+        parsley = { 'parsley-validate': true,
+                    'parsley-error-class': 'has-feedback has-error',
+                    'parsley-success-class': 'has-feedback has-success',
+                    'parsley-errors-wrapper': '<span class="help-block">',
+                    'parsley-error-template': '<div></div>',
+                    'parsley-trigger': 'focusout' }
+
+        simple_form_for(object, *(args << options.merge(html: { data: parsley })), &block)
       end
     end
   end
