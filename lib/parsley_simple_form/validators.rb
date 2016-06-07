5a260172e44f81a00ac34360c721afadf4175b5b
Michael Tratz <michael@esosoft.com>
2016-06-07 14:00:30 -0700

    Added numericality to the validator.

--- lib/parsley_simple_form/validators.rb
+++ lib/parsley_simple_form/validators.rb
@@ -1,3 +1,4 @@
 require 'parsley_simple_form/validators/base'
 require 'parsley_simple_form/validators/presence'
 require 'parsley_simple_form/validators/length'
+require 'parsley_simple_form/validators/numericality'
