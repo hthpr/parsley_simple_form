7bc919353e4d9524e592adac2f67fc1dab063728
Michael Tratz <michael@esosoft.com>
2016-06-20 17:12:37 -0700

    Made parsley_equalto validator compatible with Rails

--- lib/parsley_simple_form/validators/parsley_equalto.rb
+++ lib/parsley_simple_form/validators/parsley_equalto.rb
@@ -1,12 +1,20 @@
-class ParsleyEqualtoValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-  end
+# frozen_string_literal: true
+require 'active_model'
+require 'active_model/validator'
 
-  def attribute_validate(*args)
-    options = args.extract_options!
-    options[:message] = :confirmation
-    { 'parsley-equalto': "##{options[:object].model_name.singular}_#{options[:attribute_name].to_s.chomp '_confirmation'}",
-      'parsley-equalto-message': parsley_error_message(options),
-      'parsley-trigger': 'change focusout' }
+module ParsleyEqualto
+  class Validator < ActiveModel::EachValidator
+    def validate_each(record, attribute, value)
+    end
+
+    def attribute_validate(*args)
+      options = args.extract_options!
+      options[:message] = :confirmation
+      { 'parsley-equalto': "##{options[:object].model_name.singular}_#{options[:attribute_name].to_s.chomp '_confirmation'}",
+        'parsley-equalto-message': parsley_error_message(options),
+        'parsley-trigger': 'change focusout' }
+    end
   end
 end
+
+ActiveModel::Validations::ParsleyEqualtoValidator = ParsleyEqualto::Validator
