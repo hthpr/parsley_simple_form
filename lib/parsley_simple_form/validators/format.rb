e6dcf9fa96dc335d8f3cf48af869b6141685ac5c
Michael Tratz <michael@esosoft.com>
2016-06-22 11:15:06 -0700

    Typo extract values first before running first()

--- lib/parsley_simple_form/validators/format.rb
+++ lib/parsley_simple_form/validators/format.rb
@@ -5,7 +5,7 @@ module ParsleySimpleForm
         options = args.extract_options!
         options[:message] = :invalid
         validate = options[:validate].options
-        regexp = validate.slice(:with, :without).first.values.to_javascript
+        regexp = validate.slice(:with, :without).values.first.to_javascript
         if validate[:with]
           { "parsley-pattern": regexp, "parsley-pattern-message": parsley_error_message(options) }
         elsif validate[:without]
